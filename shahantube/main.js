b0437ca39339cf45f967d8d8e4e559d60065dbac
Shahan Neda <shahan.neda@gmail.com>
2018-02-24 23:37:50 -0800

    Restyled

--- shahantube/main.js
+++ shahantube/main.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
-	$('#search-box').keyup(function(){
-		var queary = $('#search-box').val();
+	$('#search').keyup(function(){
+		var queary = $('#search').val();
 		if(queary == ""){
 			queary = "shahan nedadahandeh"
 		}
@@ -20,10 +20,10 @@ $(document).ready(function(){
 	});
 
 
-	$(document).delegate('.videoListing', 'click', function()
-	{
-	
-		var string = 'https://www.youtube.com/embed/'+ $(this).data("id") + "?&showinfo=0&modestbranding=1&autoplay=1&loop=1&playlist="+$(this).data("id");
+	$("#search-results").on("click",".videoListing", function(){
+
+		var id =  $(this).children("div").attr("data-id");
+		var string = 'https://www.youtube.com/embed/'+ id + "?&showinfo=0&modestbranding=1&autoplay=1&loop=1&playlist="+id;
 		$('#ytplayer').attr('src', string)
 	});
 
@@ -38,25 +38,45 @@ function getRequest(searchTerm) {
         q: searchTerm,
         type:'video'
     };
-  
+
     $.getJSON(url, params, function (searchTerm) {
         showResults(searchTerm);
     });
 }
 
 function showResults(results) {
-    var html = "";
+    var html = '<div class="row">';
     var entries = results.items;
     console.log(entries)
+
     $.each(entries, function (index, value) {
         var title = value.snippet.title;
         var id = value.id.videoId;
         var thumbnail = value.snippet.thumbnails.default.url;
-        html += '<div class="videoListing" data-id="'+ id +'">'
-        html += '<p class="videoTitle">' + title + '</p>';
-        html += '<img class="videoImage" src="' + thumbnail + '">';
-        html +=	'</div>'
-    }); 
-    
+				var description = value.snippet.description;
+				if(title.length > 40){
+
+				}
+
+					html +='<div class="col s12 m6 l3 videoListing">\
+					<div class="card hoverable " data-id='+id+'>\
+					<div class="card-image">\
+				<img src="'+thumbnail+'">\
+						<span class="card-title  blue lighten-2 truncate">'+title+'</span>\
+					</div>\
+					<div class="card-content">\
+						<p class="">'+description+'</p>\
+					</div>\
+				</div></div>'
+
+
+
+
+        // html += '<div class="videoListing" data-id="'+ id +'">'
+        // html += '<p class="videoTitle">' + title + '</p>';
+        // html += '<img class="videoImage" src="' + thumbnail + '">';
+        // html +=	'</div>'
+    });
+		html+="</div>"
     $('#search-results').html(html);
 }
